d176b097206ed452b449221b997abcf4fdb65a7f
sathya reddy <sathyareddy22@outlook.com>
2024-05-11 16:08:30 +0530

    added the instance details

--- terraform/ec2/ec2.tf
+++ terraform/ec2/ec2.tf
@@ -1,4 +1,15 @@
 # resource <resource-type> <resource-name>
+resource "aws_instance" "db" {
+
+    ami = "ami-090252cbe067a9e58"
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+    instance_type = "t3.micro"
+
+    tags = {
+      Name = "db"
+    }
+}
+
 resource "aws_security_group" "allow_ssh" {
     name = "allow_ssh"
     description = "allowing SSH access"
@@ -21,3 +32,5 @@ resource "aws_security_group" "allow_ssh" {
         CreatedBy = "sathya"
     }
 }
+
+
