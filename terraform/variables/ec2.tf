94e5431189963ff666e76ac33c2ec3a4a143e860
sathya reddy <sathyareddy22@outlook.com>
2024-05-12 11:42:36 +0530

    updated the script

--- terraform/variables/ec2.tf
+++ terraform/variables/ec2.tf
@@ -1,36 +1,34 @@
 # resource <resource-type> <resource-name>
 resource "aws_instance" "db" {
 
-    ami = "var.image_id"
+    ami = var.image_id
     vpc_security_group_ids = [aws_security_group.allow_ssh.id]
     instance_type = var.instance_type
-
-    tags = {
-      Name = var.tags
-    }
+    # left side things are called as arguements, right side are values.
+    tags = var.tags
 }
 
 resource "aws_security_group" "allow_ssh" {
-    name = "allow_ssh"
-    description = "allowing SSH access"
+    name = var.sg_name
+    description = var.sg_description
 
+    # this is block
     ingress {
-        from_port        = 22
-        to_port          = 22
-        protocol         = "tcp"
-        cidr_blocks      = ["0.0.0.0/0"]
+        from_port        = var.ssh_port
+        to_port          = var.ssh_port
+        protocol         = var.protocol
+        cidr_blocks      = var.allowed_cidr
     }
+
     egress {
-        from_port        = 0
+        from_port        = 0 # from 0 to 0 means, opening all protocols
         to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-    }    
+        protocol         = "-1" # -1 all protocols
+        cidr_blocks      = var.allowed_cidr
+    }
 
     tags = {
         Name = "allow_ssh"
-        CreatedBy = "sathya"
+        CreatedBy = "Sathya_reddy"
     }
 }
-
-
--- terraform/variables/variables.tf
+++ terraform/variables/variables.tf
@@ -1,11 +1,16 @@
+#1. command line
+#2. tfvars
+#3. ENV variable
+#4. variable default value
 variable "image_id" {
-    type = string
-    default = "ami-090252cbe067a9e58"
-    description = "RHEL-9 AMI ID"  
+  type        = string #optional
+  default     = "ami-090252cbe067a9e58" #optional
+  description = "RHEL-9 AMI ID" #optional
 }
-variable "instance_type"{
-    type = string
+
+variable "instance_type" {
     default = "t3.micro"
+    type = string
 }
 
 variable "tags" {
@@ -15,5 +20,25 @@ variable "tags" {
         Module = "DB"
         Name = "DB"
     }
-  
+}
+
+variable "sg_name" {
+    default = "allow_ssh"
+}
+
+variable "sg_description" {
+    default = "allowing port 22"
+}
+
+variable "ssh_port" {
+    default = 22
+}
+
+variable "protocol" {
+    default = "tcp"
+}
+
+variable "allowed_cidr" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
 }
