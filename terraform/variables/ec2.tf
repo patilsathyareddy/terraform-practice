69fd98da94a2929f4aa0700f8ae4d1d56ee72f73
sathya reddy <sathyareddy22@outlook.com>
2024-05-12 06:51:46 +0530

    updated the script

---
+++ terraform/variables/ec2.tf
@@ -0,0 +1,36 @@
+# resource <resource-type> <resource-name>
+resource "aws_instance" "db" {
+
+    ami = "var.image_id"
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+    instance_type = var.instance_type
+
+    tags = {
+      Name = var.tags
+    }
+}
+
+resource "aws_security_group" "allow_ssh" {
+    name = "allow_ssh"
+    description = "allowing SSH access"
+
+    ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }    
+
+    tags = {
+        Name = "allow_ssh"
+        CreatedBy = "sathya"
+    }
+}
+
+
---
+++ terraform/variables/variables.tf
@@ -0,0 +1,19 @@
+variable "image_id" {
+    type = string
+    default = "ami-090252cbe067a9e58"
+    description = "RHEL-9 AMI ID"  
+}
+variable "instance_type"{
+    type = string
+    default = "t3.micro"
+}
+
+variable "tags" {
+    default = {
+        Project = "Expense"
+        Environment = "Dev"
+        Module = "DB"
+        Name = "DB"
+    }
+  
+}
